35c68490798aecfaad088655c2d031da73d4f725
lillib123 <lblair2@emich.edu>
2020-06-29 15:56:32 -0400

    a little refactor

--- src/pencil.py
+++ src/pencil.py
@@ -14,12 +14,15 @@ class Pencil:
             current_character = text[i]
             if self.durability > 0:
                 self.update_paper(current_character)
-                if current_character.isupper():
-                    self.durability -= 2
-                elif current_character.islower():
-                    self.durability -= 1
             else:
                 self.update_paper(" ")
+            self.degrade_pencil(current_character)
+
+    def degrade_pencil(self, character):
+        if character.isupper():
+            self.durability -= 2
+        elif character.islower():
+            self.durability -= 1
 
     @staticmethod
     def update_paper(character):
@@ -31,15 +34,18 @@ class Pencil:
             self.durability = self.initial_durability
 
     def erase(self, erase_text, new_text=""):
-        replacement_index = 0;
+        replacement_index = 0
+
         if re.search(r"" + erase_text + "(?!.*" + erase_text + ")", paper.get("text")):
             replacement_index = re.search(r"" + erase_text + "(?!.*" + erase_text + ")", paper.get("text")).start()
+
         for i in range(len(erase_text)):
             current_character = erase_text[::-1][i]
             if self.eraser_durability > 0 and current_character != " ":
                 replacement_text = re.sub(r"" + current_character + "(?!.*" + current_character + ")", " ", paper.get("text"))
                 paper.update({"text": replacement_text})
                 self.eraser_durability -= 1
+
         for i in range(len(new_text)):
             replacement_character = new_text[i]
             if paper.get("text")[replacement_index] != " ":
@@ -48,5 +54,5 @@ class Pencil:
                 replacement_text = paper.get("text")[:replacement_index] + replacement_character + paper.get("text")[replacement_index + 1:]
                 paper.update({"text": replacement_text})
                 replacement_index += 1
-                self.durability -= 1
+                self.degrade_pencil(replacement_character)
 
